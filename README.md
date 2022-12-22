65c48e272abb5a8f937537401cca7c3261b0cca1
prains <sleskov2013@yandex.ru>
2022-12-22 12:00:29 +0300

    123

--- README.md
+++
@@ -1,6 +0,0 @@
-Onlie shop made on React.JS and coonects to wordpress with WP Rest Api.
-Like, add to cart, or buy something on prains.github.io/thalia-react
-
-
-
-review commit
--- src/components/Home/Ropes.js
+++
@@ -1,9 +0,0 @@
-import '../../css/ropes.scss'
-
-const Ropes = (props) => {
-    return (
-        <img src={props.src} alt="" className={props.class} />
-    );
-}
-
-export default Ropes;
