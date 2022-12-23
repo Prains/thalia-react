6a12147455f6606294ea769628dc5f0225062ea6
prains <sleskov2013@yandex.ru>
2022-12-24 01:05:49 +0300

    form styling and standart validation integration

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ import useFetch from './hooks/useFetch';
 
 
 function App() {
-  let { data: temp } = useFetch('https://thaliastudio.ru/wp-json/wp/v2/product')
+  let { data: temp } = useFetch('https://thaliastudio.ru/wp-json/wp/v2/product?per_page=100')
   let { data: maintemp } = useFetch('https://thaliastudio.ru/wp-json/wp/v2/pages/7')
 
   if (localStorage.getItem('liked') === null) {
@@ -24,52 +24,52 @@ function App() {
   if (localStorage.getItem('ordered') === null) {
     localStorage.setItem('ordered', 'null')
   }
-    return (
-      <Router>
-        <div className="App">
-          {maintemp && <Header />}
-          <Switch>
-            <Route exact path='/'>
-              {maintemp == null && <Preloader />}
-              {maintemp && <Home maintemp={maintemp} />}
-            </Route>
-            <Route path='/paymentndelivery'>
-              <Paymentndelivery />
-            </Route>
-            <Route path='/contacts'>
-              <Contacts />
-            </Route>
-            <Route path='/novelties'>
-              <Novelties />
-            </Route>
-            <Route path='/favourites'>
+  return (
+    <Router>
+      <div className="App">
+        {maintemp && <Header />}
+        <Switch>
+          <Route exact path='/'>
+            {maintemp == null && <Preloader />}
+            {maintemp && <Home maintemp={maintemp} />}
+          </Route>
+          <Route path='/paymentndelivery'>
+            <Paymentndelivery />
+          </Route>
+          <Route path='/contacts'>
+            <Contacts />
+          </Route>
+          <Route path='/novelties'>
+            <Novelties />
+          </Route>
+          <Route path='/favourites'>
+            {temp == null && <Preloader />}
+            {temp && <Favourites temp={temp} />}
+          </Route>
+          <Route path='/order'>
+            {temp == null && <Preloader />}
+            {temp && <Order temp={temp} />}
+          </Route>
+          <Route path='/catalog'>
+            {temp == null && <Preloader />}
+            {temp && <Catalog temp={temp} />}
+          </Route>
+          {temp && temp.map((item) => {
+            return <Route path={`/card/${item.product_code}`} key={item.id}>
               {temp == null && <Preloader />}
-              {temp && <Favourites temp={temp} />}
+              {temp && <CatalogCard temp={item} url={item.product_code} />}
             </Route>
-            <Route path='/order'>
-              {temp == null && <Preloader />}
-              {temp && <Order temp={temp} />}
-            </Route>
-            <Route path='/catalog'>
-              {temp == null && <Preloader />}
-              {temp && <Catalog temp={temp} />}
-            </Route>
-            {temp && temp.map((item) => {
-              return <Route path={`/card/${item.product_code}`} key={item.id}>
-                {temp == null && <Preloader />}
-                {temp && <CatalogCard temp={item} url={item.product_code} />}
-              </Route>
-            })
-            }
-            <Route path='*'>
-              {maintemp == null && <Preloader />}
-              {maintemp && <Home maintemp={maintemp} />}
-            </Route>
-          </Switch>
-          {maintemp && <Footer />}
-        </div>
-      </Router>
-    );
-  }
+          })
+          }
+          <Route path='*'>
+            {maintemp == null && <Preloader />}
+            {maintemp && <Home maintemp={maintemp} />}
+          </Route>
+        </Switch>
+        {maintemp && <Footer />}
+      </div>
+    </Router>
+  );
+}
 
-  export default App;
+export default App;
--- src/pages/Order/Form/Form.js
+++ src/pages/Order/Form/Form.js
@@ -1,19 +1,23 @@
 import { arrow } from '../../../components/Icons/Icons'
 import emailjs from '@emailjs/browser';
-import { useRef } from 'react'
-
+import { useRef, useState } from 'react'
+import './Form.scss'
 const Form = (props) => {
     const form = useRef();
+    const [isPending, setPending] = useState(false);
+    let [textpend, setText] = useState('Загружается');
     const sendEmail = (e) => {
         e.preventDefault();
-
+        setPending(true);
         emailjs.sendForm('service_or27jwm', 'template_6d7rhww', form.current, 'oVC29m1gCFrYp9F5t')
             .then((result) => {
                 console.log(result.text);
+                setText('Успешно!')
             }, (error) => {
                 console.log(error.text);
-            });
-        props.setShown(!props.popupShown)
+                setText('Ошибка. Попробуйте еще раз позже')
+            })
+            .finally(() => { setTimeout(() => { props.setShown(!props.popupShown); setPending(false) }, 1500) });
     };
     let orderlist = [];
     let priceAmount = 0;
@@ -30,21 +34,22 @@ const Form = (props) => {
                     <h2 className="overlay__block__text__title title">
                         {props.title}
                     </h2>
-                    <form className='overlay__form' onSubmit={sendEmail} ref={form}>
-                        <input type="text" placeholder='Ваше имя' name="user_name" />
-                        <input type="email" placeholder='Ваша почта' name="user_email" />
-                        <input type="phone" placeholder='Ваш телефон' name="user_phone" />
+                    <form className='overlay__form' onSubmit={sendEmail} ref={form} >
+                        <input type="text" placeholder='Ваше имя' name="user_name" required minLength={2} />
+                        <input type="email" placeholder='Ваша почта' name="user_email" required minLength={4} />
+                        <input type="phone" placeholder='Ваш телефон' name="user_phone" required minLength={10} />
                         <input type="hidden" value={orderlist} name='user_list' />
                         <input type="hidden" value={priceAmount} name='user_amount' />
-                        <button type="submit">Отправить</button>
+                        <button type="submit" >Отправить</button>
                     </form>
+                    {isPending && <p className={`status ${textpend === 'Успешно' ? 'status_ok' : 'status_bad'}`}>{textpend}</p>}
                 </div>
                 <p className="catal__center__text text pointer overlay__block__link" onClick={() => { props.setShown(!props.popupShown) }}>
                     <img src={arrow} alt="arrowimg" className="" />
                     Скрыть
                 </p>
             </div>
-        </div>
+        </div >
     );
 }
 
